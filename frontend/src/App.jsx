93ec9d8d8201b93f98ad802175f1029ca9daca5a
MrudulDhaduk <dhadukmrudul@gmail.com>
2025-09-04 00:12:49 +0530

    Implemented profile page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import MutualFundDashboard from './components/Dashboard/MutualFundDashboard';
 import MutualFunds from './components/Dashboard/MutualFunds';
 import SipCalculator from './components/Home/SipCalculator';
 import GoalTracker from './components/Dashboard/GoalTracker';
+import Profile from './components/Auth/Profile';
 
 const App = () => (
   <Router>
@@ -19,6 +20,7 @@ const App = () => (
       <Route path='/MutualFunds' element={<MutualFunds />}></Route>
       <Route path="/SipCalculator" element={<SipCalculator />} />
       <Route path="/GoalTracker" element={<GoalTracker />} />
+      <Route path="/profile" element={<Profile />} />
     </Routes>
   </Router>
 );
