a2cc7973fe0bb7166e9bf20c7c3531c514ecc85e
Himanshu090705 <himanshujagtap369@gmail.com>
2025-09-04 22:24:24 +0530

    Implemented a chatbot

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -3,9 +3,11 @@ import userRoutes from "./userRoutes";
 import authRoutes from "./authRoutes";
 import { generateOTP, verifyOTP } from "../controllers/authController";
 import investmentRoutes from "./investmentsRoutes";
+import { chatController } from "../controllers/chatController";
 const routes = express.Router();
 
 routes.use("/users", userRoutes);
 routes.use("/auth", authRoutes);
-routes.use('/investments', investmentRoutes);
+routes.use("/investments", investmentRoutes);
+routes.post("/chat", chatController);
 export default routes;
